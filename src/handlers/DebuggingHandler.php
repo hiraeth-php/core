c1d0bafc276d6adf9e79c34f74ec689486fa7197
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-03-23 16:52:13 -0700

    Don't change response

--- src/handlers/DebuggingHandler.php
+++ src/handlers/DebuggingHandler.php
@@ -37,7 +37,7 @@ class DebuggingHandler extends DebugHandler
 	public function handleException($exception): int
 	{
 		if ($this->app->isDebugging()) {
-			header('HX-Reswap: innerHTML', TRUE, 200);
+			header('HX-Reswap: innerHTML', TRUE, 500);
 			header('HX-Retarget: body');
 
 			return parent::handleException($exception);
